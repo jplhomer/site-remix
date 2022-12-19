6061b38c6fe4393e64a820d7b45dfcd360e70375
Josh Larson <jplhomer@gmail.com>
2022-12-18 20:24:29 -0600

    Add basic layout

---
+++ app/components/Layout.tsx
@@ -0,0 +1,20 @@
+import type { ReactNode } from "react";
+import { Header } from "~/components/Header";
+import { Footer } from "~/components/Footer";
+
+export function Layout({ children }: { children: ReactNode }) {
+  return (
+    <>
+      <div className="fixed inset-0 flex justify-center sm:px-8">
+        <div className="flex w-full max-w-7xl lg:px-8">
+          <div className="w-full bg-white ring-1 ring-zinc-100 dark:bg-zinc-900 dark:ring-zinc-300/20" />
+        </div>
+      </div>
+      <div className="relative">
+        <Header />
+        <main>{children}</main>
+        <Footer />
+      </div>
+    </>
+  );
+}
--- app/root.tsx
+++ app/root.tsx
@@ -7,6 +7,7 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import { Layout } from "./components/Layout";
 import styles from "./tailwind.css";
 
 export const meta: MetaFunction = () => ({
@@ -17,15 +18,53 @@ export const meta: MetaFunction = () => ({
 
 export const links: LinksFunction = () => [{ rel: "stylesheet", href: styles }];
 
+const modeScript = `
+  let darkModeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)')
+
+  updateMode()
+  darkModeMediaQuery.addEventListener('change', updateModeWithoutTransitions)
+  window.addEventListener('storage', updateModeWithoutTransitions)
+
+  function updateMode() {
+    let isSystemDarkMode = darkModeMediaQuery.matches
+    let isDarkMode = window.localStorage.isDarkMode === 'true' || (!('isDarkMode' in window.localStorage) && isSystemDarkMode)
+
+    if (isDarkMode) {
+      document.documentElement.classList.add('dark')
+    } else {
+      document.documentElement.classList.remove('dark')
+    }
+
+    if (isDarkMode === isSystemDarkMode) {
+      delete window.localStorage.isDarkMode
+    }
+  }
+
+  function disableTransitionsTemporarily() {
+    document.documentElement.classList.add('[&_*]:!transition-none')
+    window.setTimeout(() => {
+      document.documentElement.classList.remove('[&_*]:!transition-none')
+    }, 0)
+  }
+
+  function updateModeWithoutTransitions() {
+    disableTransitionsTemporarily()
+    updateMode()
+  }
+`;
+
 export default function App() {
   return (
-    <html lang="en">
+    <html className="h-full antialiased" lang="en">
       <head>
         <Meta />
         <Links />
+        <script dangerouslySetInnerHTML={{ __html: modeScript }} />
       </head>
-      <body>
-        <Outlet />
+      <body className="flex h-full flex-col bg-zinc-50 dark:bg-black dark:text-zinc-100">
+        <Layout>
+          <Outlet />
+        </Layout>
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
