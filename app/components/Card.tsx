f534a217335cd1bc848071e92df90ff1cefc5e71
Josh Larson <jplhomer@gmail.com>
2022-12-22 16:04:12 -0600

    Update Uses

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -41,13 +41,26 @@ Card.Link = function CardLink({
   children: React.ReactNode;
   to: string;
 } & React.ComponentProps<typeof Link>) {
+  const innerChild = (
+    <>
+      <span className="absolute -inset-y-6 -inset-x-4 z-20 sm:-inset-x-6 sm:rounded-2xl" />
+      <span className="relative z-10">{children}</span>
+    </>
+  );
+  const isExternalLink = to.startsWith("http");
+
   return (
     <>
       <div className="absolute -inset-y-6 -inset-x-4 z-0 scale-95 bg-zinc-50 opacity-0 transition group-hover:scale-100 group-hover:opacity-100 dark:bg-zinc-800/50 sm:-inset-x-6 sm:rounded-2xl" />
-      <Link to={to} prefetch="intent" {...props}>
-        <span className="absolute -inset-y-6 -inset-x-4 z-20 sm:-inset-x-6 sm:rounded-2xl" />
-        <span className="relative z-10">{children}</span>
-      </Link>
+      {isExternalLink ? (
+        <a href={to} {...props}>
+          {innerChild}
+        </a>
+      ) : (
+        <Link to={to} prefetch="intent" {...props}>
+          {innerChild}
+        </Link>
+      )}
     </>
   );
 };
--- app/routes/uses.tsx
+++ app/routes/uses.tsx
@@ -1,30 +1,6 @@
-import { MetaFunction } from "@remix-run/cloudflare";
-import Head from "next/head";
-
 import { Card } from "~/components/Card";
 import { Section } from "~/components/Section";
 import { SimpleLayout } from "~/components/SimpleLayout";
-
-function ToolsSection({ children, ...props }) {
-  return (
-    <Section {...props}>
-      <ul role="list" className="space-y-16">
-        {children}
-      </ul>
-    </Section>
-  );
-}
-
-function Tool({ title, href, children }) {
-  return (
-    <Card as="li">
-      <Card.Title as="h3" href={href}>
-        {title}
-      </Card.Title>
-      <Card.Description>{children}</Card.Description>
-    </Card>
-  );
-}
 
 export function meta() {
   return {
@@ -37,52 +13,38 @@ export function meta() {
 export default function Uses() {
   return (
     <SimpleLayout
-      title="Software I use, gadgets I love, and other things I recommend."
-      intro="I get asked a lot about the things I use to build software, stay productive, or buy to fool myself into thinking I’m being productive when I’m really just procrastinating. Here’s a big list of all of my favorite stuff."
+      title="The things I use"
+      intro="Some poeple are interested in the things other people use to do their daily work. Here is one of those lists."
     >
       <div className="space-y-20">
         <ToolsSection title="Workstation">
-          <Tool title="16” MacBook Pro, M1 Max, 64GB RAM (2021)">
-            I was using an Intel-based 16” MacBook Pro prior to this and the
-            difference is night and day. I’ve never heard the fans turn on a
-            single time, even under the incredibly heavy loads I put it through
-            with our various launch simulations.
+          <Tool title="13” MacBook Air, M1, Rose Gold, 16GB RAM (2020)">
+            M1 is great. I don't even need all the RAM I had before. It feels
+            like the battery lasts all week, and it's super lightweight and
+            portable. Love the color, too. YOLO.
           </Tool>
-          <Tool title="Apple Pro Display XDR (Standard Glass)">
-            The only display on the market if you want something HiDPI and
-            bigger than 27”. When you’re working at planetary scale, every pixel
-            you can get counts.
+          <Tool title="TODO: 4K Monitor">
+            My pals at Shopify sent me this, and it's really nice to have a High
+            DPI display to work on.
           </Tool>
-          <Tool title="IBM Model M SSK Industrial Keyboard">
-            They don’t make keyboards the way they used to. I buy these any time
-            I see them go up for sale and keep them in storage in case I need
-            parts or need to retire my main.
+          <Tool title="Regular Apple Keyboard">
+            I'm not a keyboard snob. It gets the job done.
           </Tool>
-          <Tool title="Apple Magic Trackpad">
-            Something about all the gestures makes me feel like a wizard with
-            special powers. I really like feeling like a wizard with special
-            powers.
+          <Tool title="USB Mouse">
+            It's not even that good. Shopify sent it to me, and it allows me to
+            point and click on things.
           </Tool>
-          <Tool title="Herman Miller Aeron Chair">
-            If I’m going to slouch in the worst ergonomic position imaginable
-            all day, I might as well do it in an expensive chair.
+          <Tool title="TODO Standing Desk">
+            It's very, very nice. I used to have a white Autonomous, but this
+            one feels much more solid and "at home" than the corporate-feeling
+            Autonomous desk.
           </Tool>
         </ToolsSection>
         <ToolsSection title="Development tools">
-          <Tool title="Sublime Text 4">
-            I don’t care if it’s missing all of the fancy IDE features everyone
-            else relies on, Sublime Text is still the best text editor ever
-            made.
+          <Tool title="VSCode">
+            It's dope. Works well for my Laravel and React projects.
           </Tool>
-          <Tool title="iTerm2">
-            I’m honestly not even sure what features I get with this that aren’t
-            just part of the macOS Terminal but it’s what I use.
-          </Tool>
-          <Tool title="TablePlus">
-            Great software for working with databases. Has saved me from
-            building about a thousand admin interfaces for my various projects
-            over the years.
-          </Tool>
+          <Tool title="Hyper">It's a neat terminal made by Vercel.</Tool>
         </ToolsSection>
         <ToolsSection title="Design">
           <Tool title="Figma">
@@ -92,27 +54,46 @@ export default function Uses() {
           </Tool>
         </ToolsSection>
         <ToolsSection title="Productivity">
-          <Tool title="Alfred">
-            It’s not the newest kid on the block but it’s still the fastest. The
-            Sublime Text of the application launcher world.
+          <Tool title="LogSeq">
+            I use it for journaling and note-taking. It’s a great tool for TODOs
+            and keeping track of things I want to do. Very helpful for work.
           </Tool>
-          <Tool title="Reflect">
-            Using a daily notes system instead of trying to keep things
-            organized by topics has been super powerful for me. And with
-            Reflect, it’s still easy for me to keep all of that stuff
-            discoverable by topic even though all of my writing happens in the
-            daily note.
-          </Tool>
-          <Tool title="SavvyCal">
-            Great tool for scheduling meetings while protecting my calendar and
-            making sure I still have lots of time for deep work during the week.
-          </Tool>
-          <Tool title="Focus">
-            Simple tool for blocking distracting websites when I need to just do
-            the work and get some momentum going.
+          <Tool title="Reminders (iOS)">
+            I ask Siri to add things to my reminders, and they remind me to do
+            so.
           </Tool>
         </ToolsSection>
       </div>
     </SimpleLayout>
+  );
+}
+
+function ToolsSection({
+  children,
+  ...props
+}: { children: React.ReactNode } & React.ComponentProps<typeof Section>) {
+  return (
+    <Section {...props}>
+      <ul className="space-y-16">{children}</ul>
+    </Section>
+  );
+}
+
+function Tool({
+  title,
+  href,
+  children,
+}: {
+  title: string;
+  href?: string;
+  children: React.ReactNode;
+}) {
+  return (
+    <Card as="li">
+      <Card.Title as="h3" to={href}>
+        {title}
+      </Card.Title>
+      <Card.Description>{children}</Card.Description>
+    </Card>
   );
 }
